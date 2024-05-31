908647e62ef00edda96616eba5d57f123c9aae82
Dan Gough <goughdj@gmail.com>
2024-05-31 10:01:25 +0200

    Fix 8x8 Work, add exe variant

--- Nevergreen/Apps/Get-8x8Work.ps1
+++ Nevergreen/Apps/Get-8x8Work.ps1
@@ -1,5 +1,13 @@
-$URL = Get-Link -Uri 'https://support.8x8.com/cloud-phone-service/voice/work-desktop/download-8x8-work-for-desktop' -MatchProperty href -Pattern '\.msi'
+try {
+    $Content = (Invoke-RestMethod -Uri 'https://support-portal.8x8.com/helpcenter/docrenderservice/services/rest/documents/8bff4970-6fbf-4daf-842d-8ae9b533153d?mandatorKey=MANDATOR_USU&contentLanguage=en&additionalData=references=true&additionalData=referencedDocs=true&additionalData=withGraph=true&additionalData=includeReferencesUpToLevel=all' -DisableKeepAlive).Content
 
-$Version = $URL | Get-Version -ReplaceWithDot
+    $MsiUrl = ($Content | Select-String -Pattern 'https://[^<]+\.msi').Matches.Value | Select-Object -First 1
+    $ExeUrl = ($Content | Select-String -Pattern 'https://[^<]+\.exe').Matches.Value | Select-Object -First 1
+    $Version = $URL | Get-Version -ReplaceWithDot
 
-New-NevergreenApp -Name '8x8 Work' -Version $Version -Uri $URL -Architecture 'x64' -Type 'msi'
+    New-NevergreenApp -Name '8x8 Work' -Version $Version -Uri $MsiUrl -Architecture 'x64' -Type 'MSI'
+    New-NevergreenApp -Name '8x8 Work' -Version $Version -Uri $ExeUrl -Architecture 'x64' -Type 'Exe'
+}
+catch {
+    Write-Error "$($MyInvocation.MyCommand): $($_.Exception.Message)"
+}
