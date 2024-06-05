f0f90f129581762dc96596ca6d748236fbce3b15
Dan Gough <goughdj@gmail.com>
2024-06-05 10:10:03 +0100

    Fix 8x8 Work

--- Nevergreen/Apps/Get-8x8Work.ps1
+++ Nevergreen/Apps/Get-8x8Work.ps1
@@ -1,12 +1,18 @@
+$Apps = @(
+    @{Name = '8x8 Work'; Architecture = 'x64'; Type = 'Exe'; Pattern = 'https://[^<]+\.exe' }
+    @{Name = '8x8 Work'; Architecture = 'x64'; Type = 'MSI'; Pattern = 'https://[^<]+\.msi' }
+)
+
 try {
+    # Download page https://support-portal.8x8.com/viewArticle.html?d=8bff4970-6fbf-4daf-842d-8ae9b533153d&hl=en references content below
     $Content = (Invoke-RestMethod -Uri 'https://support-portal.8x8.com/helpcenter/docrenderservice/services/rest/documents/8bff4970-6fbf-4daf-842d-8ae9b533153d?mandatorKey=MANDATOR_USU&contentLanguage=en&additionalData=references=true&additionalData=referencedDocs=true&additionalData=withGraph=true&additionalData=includeReferencesUpToLevel=all' -DisableKeepAlive).Content
 
-    $MsiUrl = ($Content | Select-String -Pattern 'https://[^<]+\.msi').Matches.Value | Select-Object -First 1
-    $ExeUrl = ($Content | Select-String -Pattern 'https://[^<]+\.exe').Matches.Value | Select-Object -First 1
-    $Version = $URL | Get-Version -ReplaceWithDot
+    foreach ($App in $Apps) {
+        $Url = ($Content | Select-String -Pattern $App.Pattern).Matches.Value | Select-Object -First 1
+        $Version = $Url | Get-Version -ReplaceWithDot
+        New-NevergreenApp -Name $App.Name -Version $Version -Uri $Url -Architecture $App.Architecture -Type $App.Type
+    }
 
-    New-NevergreenApp -Name '8x8 Work' -Version $Version -Uri $MsiUrl -Architecture 'x64' -Type 'MSI'
-    New-NevergreenApp -Name '8x8 Work' -Version $Version -Uri $ExeUrl -Architecture 'x64' -Type 'Exe'
 }
 catch {
     Write-Error "$($MyInvocation.MyCommand): $($_.Exception.Message)"
